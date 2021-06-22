b593136dda87923ce9306f04d3f8ee7a901d341f
1 <mihchizh@yandex.ru>
2021-06-22 23:49:26 +0300

    add new method test_add_new_contact

---
+++ Contact.py
@@ -0,0 +1,5 @@
+class Contact:
+    def __init__(self, Title,CompanyName, CompanyAdress):
+        self.Title = Title
+        self.CompanyName = CompanyName
+        self.CompanyAdress = CompanyAdress
