fd9dafcf6f1ac2a528e6d4e0f5793d5fe9e1d794
1 <mihchizh@yandex.ru>
2021-06-23 18:33:24 +0300

    return to group page moved to cteate group

--- test_add_group.py
+++ test_add_group.py
@@ -15,13 +15,11 @@ class TestAddGroup(unittest.TestCase):
         wd = self.wd
         self.Login(wd, username="admin", password="secret")
         self.create_group1(wd, Group(name="New group", header="Test header", footer="Test footer"))
-        self.return_to_group_page(wd)
         self.Logout(wd)
     def test_add_empty_group(self):
         wd = self.wd
         self.Login(wd, username="admin", password="secret")
         self.create_group1(wd, Group(name="", header="", footer=""))
-        self.return_to_group_page(wd)
         self.Logout(wd)
     def Logout(self, wd):
         # logout
@@ -43,6 +41,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_group_page(wd)
 
     def return_to_group_page(self, wd):
         # return to group page
