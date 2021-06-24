4d3004f34d2e1916ea202a896231b6532ffd2a08
1 <mihchizh@yandex.ru>
2021-06-24 17:40:51 +0300

    commit

--- Contact.py
+++ Contact.py
@@ -1,5 +1,25 @@
 class Contact:
-    def __init__(self, Title,CompanyName, CompanyAdress):
-        self.Title = Title
-        self.CompanyName = CompanyName
-        self.CompanyAdress = CompanyAdress
+    def __init__(self, firstname, middlename, lastname, nickname, title, companyname,
+                 companyadress,homephone, mobilephone, workphone, fax, email, email2, homepage, bday,
+                 bmonth, byear, group, adress2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.companyname = companyname
+        self.companyadress = companyadress
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.group = group
+        self.adress2 = adress2
+        self.phone2 = phone2
+        self.notes = notes
