79671ced21ea25edeb43d87d21f9417224953cba
1 <mihchizh@yandex.ru>
2021-06-24 23:35:25 +0300

    change home page

--- Test/add_contact.py
+++ Test/add_contact.py
@@ -12,11 +12,7 @@ def app(request):
 def test_add_contact(app):
     app.Login(username="admin", password="secret" )
     # create new contact
-    app.Create_new_contact(Contact(firstname="Miha", middlename="testov", lastname="Tetki", nickname="telega",
-        title="ret", companyname="auriga", companyadress="Russia", homephone="2222222", mobilephone="3333333",
-        workphone="1111111", fax="1234567", email="test@test.ru", email2="test1@test.ru",
-        homepage="localrussia.com", bday="2", bmonth="May", byear="1985", group="New group",
-        adress2="Russia 1", phone2="Gorky", notes="notes"))
+    app.Create_new_contact(Contact(firstname="Miha", middlename="testov", lastname="Tetki", nickname="telega", title="ret", companyname="auriga", companyadress="Russia", homephone="2222222", mobilephone="3333333", workphone="1111111", fax="1234567", email="test@test.ru", email2="test1@test.ru", homepage="localrussia.com", bday="2", bmonth="May", byear="1985", group="New group", adress2="Russia 1", phone2="Gorky", notes="notes"))
     app.Logout()
 
 def test_add_new_contact(app):
