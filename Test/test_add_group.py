8a43113679757a5fe96b57609367a39b19386525
1 <mihchizh@yandex.ru>
2021-06-25 20:38:09 +0300

    run tests

--- Test/test_add_group.py
+++ Test/test_add_group.py
@@ -2,12 +2,6 @@
 from model.group import Group
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
 def test_add_group(app):
     app.session.Login(username="admin", password="secret")
     app.group.create(Group(name="New group", header="Test header", footer="Test footer"))
