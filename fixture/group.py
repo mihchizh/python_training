69c3bfef11a54ebc5a256c16d6ab07bdb8f5a772
1 <mihchizh@yandex.ru>
2021-06-26 14:38:53 +0300

    create test group modification fix

--- fixture/group.py
+++ fixture/group.py
@@ -15,9 +15,9 @@ class GroupHelper:
 
     def fill_group_form(self, group):
         wd = self.app.wd
-        self.change_field_value("group name", group.name)
-        self.change_field_value("group header", group.header)
-        self.change_field_value("group footer", group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
