da6f8bb4bd23a64ae5ce185246d9fd313b83b7fa
1 <mihchizh@yandex.ru>
2021-06-27 20:03:50 +0300

    add list for add group.py

--- fixture/group.py
+++ fixture/group.py
@@ -91,4 +91,4 @@ class GroupHelper:
             text = element.text
             id = element.find_element_by_name("selected[]").get_attribute("value")
             groups.append(Group(name=text, id=id))
-            return groups
+        return groups
