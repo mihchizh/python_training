93930ef5ae2aab1b1f523821ed0b43405835b42e
1 <mihchizh@yandex.ru>
2021-06-27 11:11:11 +0300

    refactoring test delete Contact.py

--- Test/test_delete_contact.py
+++ Test/test_delete_contact.py
@@ -1,4 +1,8 @@
+from model.Contact import Contact
+
 
 def test_delete_first_contact(app):
+    if app.contact.count_contact() == 0:
+        app.contact.create_new_contact(Contact(firstname="test333"))
     app.contact.delete_first_contact()
 
