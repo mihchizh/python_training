0e471e762c7b363377f63374e4467127cb77e356
1 <mihchizh@yandex.ru>
2021-06-28 11:47:17 +0300

    edit first group

--- Test/test_edit_first_group.py
+++ Test/test_edit_first_group.py
@@ -2,6 +2,6 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
-    app.session.Login(username="admin", password="secret")
-    app.group.edit(Group(name="Edit Group", header="Edit header", footer="Edit footer"))
-    app.session.Logout()
+    if app.group.count() == 0:
+        app.group.create(Group(name = "test"))
+    app.group.modify_first_group(Group(name="New group1"))
