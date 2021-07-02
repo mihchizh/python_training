44b4df775501e6b3318686a32a82cdcda0e1348f
1 <mihchizh@yandex.ru>
2021-07-02 14:57:38 +0300

    add list verification in test_edit_contact.py

--- Test/test_edit_contact.py
+++ Test/test_edit_contact.py
@@ -4,4 +4,8 @@ from model.Contact import Contact
 def test_edit_first_contact(app):
     if app.contact.count_contact() == 0:
         app.contact.create_new_contact(Contact(firstname="test333"))
-    app.contact.edit_first_contact(Contact(firstname="test777"))
+    old_contacts = app.contact.get_contact_list()
+    contact = (Contact(firstname="Test777"))
+    app.contact.edit_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
