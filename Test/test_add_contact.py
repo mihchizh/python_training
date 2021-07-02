06e77823c8cc0dff027a576afd2fe1ac4d4cf0fb
1 <mihchizh@yandex.ru>
2021-07-02 14:23:49 +0300

    add verification in test add contact

--- Test/test_add_contact.py
+++ Test/test_add_contact.py
@@ -2,19 +2,22 @@ from model.Contact import Contact
 
 
 def test_add_contact(app):
-    app.contact.create_new_contact(
-        Contact(firstname="Miha", middlename="testov", lastname="Tetki", nickname="telega", title="ret",
+    old_contacts = app.contact.get_contact_list()
+    contact = (Contact(firstname="Miha", middlename="testov", lastname="Tetki", nickname="telega", title="ret",
                 company="auriga", address="Russia", home="2222222", mobile="3333333",
                 work="1111111", fax="1234567", email="test@test.ru", email2="test1@test.ru",
                 homepage="localrussia.com", bday="2", bmonth="May", byear="1985", group="New group", address2="Russia 1",
                 phone2="Gorky", notes="notes"))
+    app.contact.create_new_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
 
 
 
-def test_add_new_contact(app):
-    app.contact.create_new_contact(
-        Contact(firstname="Miha1", middlename="testov1", lastname="Tetki1", nickname="telega1", title="ret1",
-                address="Russia1", company="auriga1", home="22222221", mobile="33333331",
-                work="11111111", fax="12345671", email="test1@test.ru", email2="test1@test.ru",
-                homepage="localrussia1.com", bday="2", bmonth="May", byear="1985", group="New group",
-                address2="Russia 11", phone2="Gorky1", notes="notes1"))
+#def test_add_new_contact(app):
+   # app.contact.create_new_contact(
+       # Contact(firstname="Miha1", middlename="testov1", lastname="Tetki1", nickname="telega1", title="ret1",
+                #address="Russia1", company="auriga1", home="22222221", mobile="33333331",
+                #work="11111111", fax="12345671", email="test1@test.ru", email2="test1@test.ru",
+                #homepage="localrussia1.com", bday="2", bmonth="May", byear="1985", group="New group",
+                #address2="Russia 11", phone2="Gorky1", notes="notes1"))
