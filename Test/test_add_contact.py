fd2b42eba360da16e253fce8bdd8b704cfa288cc
1 <mihchizh@yandex.ru>
2021-07-02 14:30:02 +0300

    add verification in test add contact sorted by firstname

--- Test/test_add_contact.py
+++ Test/test_add_contact.py
@@ -11,6 +11,8 @@ def test_add_contact(app):
     app.contact.create_new_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
