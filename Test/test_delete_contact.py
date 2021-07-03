3a0d7722a5eaa11103a40f9dded8f4c2ace00a89
1 <mihchizh@yandex.ru>
2021-07-03 09:45:30 +0300

    count instead new contact

--- Test/test_delete_contact.py
+++ Test/test_delete_contact.py
@@ -3,11 +3,11 @@ from model.Contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count_contact() == 0:
-        app.contact.create_new_contact(Contact(firstname="test333"))
+        app.contact.create_new_contact(Contact(firstname="abc", lastname="dort"))
     old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    assert len(old_contacts) - 1 == app.contact.count_contact()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[0:1] = []
     assert  old_contacts == new_contacts
 
