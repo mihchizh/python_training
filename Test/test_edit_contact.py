8da26da570260b96691267d9776f9f653ef3dd5a
1 <mihchizh@yandex.ru>
2021-07-03 09:43:12 +0300

    count instead new contact

--- Test/test_edit_contact.py
+++ Test/test_edit_contact.py
@@ -7,7 +7,7 @@ def test_edit_first_contact(app):
     old_contacts = app.contact.get_contact_list()
     contact = (Contact(firstname="ABC", lastname="Baf"))
     app.contact.edit_first_contact(contact)
+    assert len(old_contacts) == app.contact.count_contact()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
