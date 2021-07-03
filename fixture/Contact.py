75e7a530e722c9483ef7b5329abfb1ebc1275090
1 <mihchizh@yandex.ru>
2021-07-03 18:23:14 +0300

    add edit_by_index and view_by_index

--- fixture/Contact.py
+++ fixture/Contact.py
@@ -101,5 +101,17 @@ class ContactHelper:
                 self.contact_cache.append(Contact(lastname=last_name,firstname=first_name, id=id_cont))
         return list(self.contact_cache)
 
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
 
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
 
