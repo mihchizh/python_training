98a109c1cbad9fe2d81cc1e4fc1ef2f57cf7f955
1 <mihchizh@yandex.ru>
2021-07-04 11:41:05 +0300

    add testdata

--- Test/test_add_group.py
+++ Test/test_add_group.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
 
 
-def test_add_group(app):
-    old_groups = app.group.get_group_list()
-    group = (Group(name="New group", header="Test header", footer="Test footer"))
-    app.group.create(group)
-    assert len(old_groups)+1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted (new_groups, key=Group.id_or_max)
+testdata = [
+        Group(name="New group", header="Test header", footer="Test footer"),
+        Group(name="", header="", footer="")]
+
+@pytest.mark.parametrize("group", testdata)
+def test_add_group(app, group):
+    pass
+    #old_groups = app.group.get_group_list()
+    #group = (Group(name="New group", header="Test header", footer="Test footer"))
+    #app.group.create(group)
+    #assert len(old_groups)+1 == app.group.count()
+    #new_groups = app.group.get_group_list()
+    #old_groups.append(group)
+    #assert sorted(old_groups, key=Group.id_or_max) == sorted (new_groups, key=Group.id_or_max)
 
 
 #def test_add_empty_group(app):
