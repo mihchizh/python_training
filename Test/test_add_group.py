44f8e59ee124521baba0f49283abdb0ca2a52f1d
1 <mihchizh@yandex.ru>
2021-07-05 20:36:44 +0300

    add data

--- Test/test_add_group.py
+++ Test/test_add_group.py
@@ -11,7 +11,7 @@ def test_add_group(app, group):
     assert len(old_groups)+1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted (new_groups, key=Group.id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 #def test_add_empty_group(app):
