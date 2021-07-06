9d02f6fae7a4467efcccdec5150a1554f4acf2c5
1 <mihchizh@yandex.ru>
2021-07-06 09:04:29 +0300

    delete clear

--- Test/test_contact.py
+++ Test/test_contact.py
@@ -3,9 +3,9 @@ import re
 def test_contact_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.firstname == clear(contact_from_edit_page.firstname)
-    assert contact_from_home_page.lastname == clear(contact_from_edit_page.lastname)
-    assert contact_from_home_page.address == clear(contact_from_edit_page.address)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
