cd73de48bb42c623bd172584c20dde8a1f1d9059
1 <mihchizh@yandex.ru>
2021-07-06 11:52:49 +0300

    add generator for test_add_contact

--- Test/test_add_contact.py
+++ Test/test_add_contact.py
@@ -1,18 +1,9 @@
 from model.Contact import Contact
-import string
-import random
-import pytest
-
-def random_string(prefix, maxlen):
-    simbols = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(simbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Contact(firstname="", lastname="")] + [Contact(firstname=random_string("firstname", 10),
-                                                            lastname=random_string("lastname", 10)) for i in range(5)]
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+
+def test_add_contact(app, json_contact):
+    contact = json_contact
     old_contacts = app.contact.get_contact_list()
     app.contact.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count_contact()
