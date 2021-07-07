18c17a150e5f31a9e14d7af6fbba299735e0cd05
1 <mihchizh@yandex.ru>
2021-07-07 10:43:20 +0300

    add check_ui for del contact

--- fixture/Contact.py
+++ fixture/Contact.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support.select import Select
 from model.Contact import Contact
 import re
 
+
 class ContactHelper:
 
     def __init__(self, app):
@@ -62,6 +63,12 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_elements_by_name("selected[]") [index].click()
 
+    def select_contact_by_id(self, id):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
+
     def delete_first_contact(self):
         self.delete_contact_by_index(0)
 
@@ -71,6 +78,16 @@ class ContactHelper:
         self.open_home_page()
         # select first contact
         self.select_contact_by_index(index)
+        # submit deletion
+        wd.find_element_by_xpath("//*[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
+        self.contact_cache = None
+
+    def delete_contact_by_id(self, id):
+        wd = self.app.wd
+        self.open_home_page()
+        # select first contact
+        self.select_contact_by_id(id)
         # submit deletion
         wd.find_element_by_xpath("//*[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
