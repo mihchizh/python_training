0bbfebd22d4988cedf91ab1d58d01a0de91ed881
1 <mihchizh@yandex.ru>
2021-07-07 09:54:43 +0300

    add check_ui for add group

--- Test/test_add_group.py
+++ Test/test_add_group.py
@@ -9,6 +9,7 @@ def test_add_group(app, db, json_groups, check_ui):
     app.group.create(group)
     new_groups = db.get_group_list()
     old_groups.append(group)
+    assert old_groups == new_groups
     if check_ui:
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
