3d4aad44552696ed4ed40e5f7c60000cc89f8b10
1 <mihchizh@yandex.ru>
2021-07-07 10:03:08 +0300

    add check_ui for add contact

--- Test/test_add_contact.py
+++ Test/test_add_contact.py
@@ -2,14 +2,15 @@ from model.Contact import Contact
 
 
 
-def test_add_contact(app, json_contact):
+def test_add_contact(app, db, json_contact, check_ui):
     contact = json_contact
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     app.contact.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count_contact()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
